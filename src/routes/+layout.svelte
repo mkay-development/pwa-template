7b37ca6444dbf47e4966e4776d0ccd15eca87948
Martz <github@jmartz.de>
2023-01-18 21:08:04 +0100

    fix service worker and update footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script>
   import "../tailwind.css";
+  import Fa from "svelte-fa";
+  import { faHeart } from "@fortawesome/free-solid-svg-icons";
 </script>
 
 <header class="mx-auto max-w-5xl my-2 flex justify-between">
-  <h1 class="text-xl font-bold">Logo</h1>
+  <h1 class="text-xl font-bold"><a href="/">Logo</a></h1>
   <nav>Test</nav>
 </header>
 
@@ -14,7 +16,14 @@
     <div class="col-span-6 md:col-span-2">
       <a href="/impressum">Impressum</a> & <a href="/dateschutz">Datenschutz</a>
     </div>
-    <div class="col-span-6 md:col-span-2">Made with by Jonathan</div>
-    <div class="col-span-6 md:col-span-2">Copyright by Jonathan Martz</div>
+    <div class="col-span-6 md:col-span-2 flex">
+      Made with <Fa icon={faHeart} class="mx-2 mt-1 text-red-400" /> by Jonathan
+    </div>
+    <div class="col-span-6 md:col-span-2">
+      Copyright by <a
+        href="https://www.mkay-development.de"
+        class="text-blue-600">MkaY Development</a
+      >
+    </div>
   </div>
 </footer>
