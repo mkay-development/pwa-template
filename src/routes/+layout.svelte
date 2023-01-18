7280a35d00bb03105aa28e0abf39d889923cbe90
Martz <github@jmartz.de>
2023-01-18 23:00:05 +0100

    update layout padding

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,14 +4,14 @@
   import { faHeart } from "@fortawesome/free-solid-svg-icons";
 </script>
 
-<header class="mx-auto max-w-5xl my-2 flex justify-between">
+<header class="mx-auto max-w-5xl my-2 flex justify-between px-2 py-2">
   <h1 class="text-xl font-bold"><a href="/">Logo</a></h1>
   <nav>Test</nav>
 </header>
 
-<main class="mx-auto max-w-5xl my-2"><slot /></main>
+<main class="mx-auto max-w-5xl my-2 px-2 py-2"><slot /></main>
 
-<footer class="mx-auto max-w-5xl my-2">
+<footer class="mx-auto max-w-5xl my-2 px-2 py-2">
   <div class="grid grid-cols-6">
     <div class="col-span-6 md:col-span-2">
       <a href="/impressum">Impressum</a> & <a href="/data-privacy">Datenschutz</a>
